dc91fb748861f7b4a98b98fe23142b58fae78a47
Joaquín Yeray Martín de la Nuez <joaquinmnsm@gmail.com>
2016-10-07 10:27:42 +0000

    TripService should throw NotLoggedInUserException when user is not logged

---
+++ c#/TripServiceKata/TripServiceKata.Tests/TripServiceShould.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using FluentAssertions;
+using NSubstitute;
+using NUnit.Framework;
+using TripServiceKata.Exception;
+using TripServiceKata.Trip;
+using TripServiceKata.User;
+
+namespace TripServiceKata.Tests
+{
+    [TestFixture]
+    public class TripServiceShould
+    {
+        [Test]
+        public void throw_NotLoggedInUserException_when_user_is_not_logged()
+        {
+            var tripService = new TesteableTripService();
+
+            Action getTripsByUserAction = () => tripService.GetTripsByUser(null);
+
+            getTripsByUserAction.ShouldThrow<UserNotLoggedInException>();
+        }
+    }
+
+    public class TesteableTripService : TripService
+    {
+        protected override User.User GetLoggedUser()
+        {
+            return null;
+        }
+    }
+}
