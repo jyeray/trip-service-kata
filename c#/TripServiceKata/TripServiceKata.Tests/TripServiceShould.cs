c0ca67797a46b8c27214107bdb8693603a0c4fb9
Joaquín Yeray Martín de la Nuez <joaquinmnsm@gmail.com>
2016-10-07 11:29:47 +0000

    TripService returns list of trips if users are friends

--- c#/TripServiceKata/TripServiceKata.Tests/TripServiceShould.cs
+++ c#/TripServiceKata/TripServiceKata.Tests/TripServiceShould.cs
@@ -14,6 +14,8 @@ namespace TripServiceKata.Tests
         private static readonly User.User AnotherUser = new User.User();
         private const User.User UnusedUser = null;
         private static User.User loggedInUser;
+        private static readonly Trip.Trip ToGranCanaria = new Trip.Trip();
+        private static readonly Trip.Trip ToMadrid = new Trip.Trip();
 
         [Test]
         public void throw_NotLoggedInUserException_when_user_is_not_logged()
@@ -37,11 +39,34 @@ namespace TripServiceKata.Tests
             anotherUserTrips.ShouldBeEquivalentTo(new List<Trip.Trip>());
         }
 
+        [Test]
+        public void returns_list_of_trips_if_users_are_friends()
+        {
+            var tripService = new TesteableTripService();
+            loggedInUser = AnUser;
+            AnotherUser.AddFriend(AnUser);
+            AnotherUser.AddTrip(ToGranCanaria);
+            AnotherUser.AddTrip(ToMadrid);
+
+            var anotherUserTrips = tripService.GetTripsByUser(AnotherUser);
+
+            anotherUserTrips.ShouldBeEquivalentTo(new List<Trip.Trip>
+            {
+                ToGranCanaria,
+                ToMadrid
+            });
+        }
+
         public class TesteableTripService : TripService
         {
             protected override User.User GetLoggedUser()
             {
                 return loggedInUser;
+            }
+
+            protected override List<Trip.Trip> FindTripsByUser(User.User user)
+            {
+                return user.Trips();
             }
         }
     }
