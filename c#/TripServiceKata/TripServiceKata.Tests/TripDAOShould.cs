26c16f0f57a304712921cfee918fe6fe28160b9e
Joaquín Yeray Martín de la Nuez <joaquinmnsm@gmail.com>
2016-10-16 16:13:05 +0000

    Test TripDAO and add a method.

---
+++ c#/TripServiceKata/TripServiceKata.Tests/TripDAOShould.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using FluentAssertions;
+using NUnit.Framework;
+using TripServiceKata.Exception;
+using TripServiceKata.Trip;
+
+namespace TripServiceKata.Tests
+{
+    [TestFixture]
+    public class TripDAOShould
+    {
+        [Test]
+        public void throw_DependendClassCallDuringUnitTestException_when_getting_users_trips()
+        {
+            var tripDao = new TripDAO();
+            Action action = () => tripDao.GetUserTrips(new User.User());
+
+            action.ShouldThrow<DependendClassCallDuringUnitTestException>();
+        }
+    }
+}
